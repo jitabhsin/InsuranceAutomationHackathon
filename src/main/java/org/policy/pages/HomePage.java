7593aa756c85b3f1d577d1a53b2238be7adc042b
V R <2497728@cognizant.com>
2026-06-30 12:07:40 +0530

    Updated

--- src/main/java/org/policy/pages/HomePage.java
+++ src/main/java/org/policy/pages/HomePage.java
@@ -1,4 +1,37 @@
 package org.policy.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class HomePage {
+    private WebDriver driver;
+
+    public HomePage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath="/html/body/main/div[2]/section/div[7]/a/div[1]/div/i")
+    public WebElement travelInsuranceElement;
+
+    @FindBy(xpath="/html/body/main/div[2]/section/div[4]/a/div[1]/div/i")
+    public WebElement carInsuranceElement;
+
+    @FindBy(xpath="/html/body/main/div[2]/section/div[6]/a/div[1]/div/i")
+    public WebElement healthInsuranceElement;
+
+    public void clickTravelInsurance(){
+        travelInsuranceElement.click();
+    }
+
+    public void clickCarInsurance(){
+        carInsuranceElement.click();
+    }
+
+    public void clickHealthInurance(){
+        healthInsuranceElement.click();
+    }
+
 }
