77f7da124a1a3b71e3056c6280ea5cf399e64c6f
Uday Patel <uday12704@gmail.com>
2026-06-30 15:36:35 +0530

    verify total plan testcase created

---
+++ src/test/java/test/TC_06_VerifyTotalPlan.java
@@ -0,0 +1,13 @@
+package test;
+
+import basetest.BaseTest;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class TC_06_VerifyTotalPlan {
+
+    @Test
+    public void testTotalPlan(){
+        WebDriver driver;
+    }
+}
