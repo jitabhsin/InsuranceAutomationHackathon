63e4fc072b8ee811791fba6060ee21440c9116a1
Patel <2497826@cognizant.com>
2026-07-01 00:57:30 +0530

    2 testcases are added

--- src/test/java/test/TC_06_VerifyTotalPlan.java
+++ src/test/java/test/TC_06_VerifyTotalPlan.java
@@ -2,15 +2,17 @@ package test;
 
 import basetest.BaseTest;
 import org.openqa.selenium.WebDriver;
+import org.policy.pages.TravelQuotePage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class TC_06_VerifyTotalPlan {
-
-    BaseTest base;
+public class TC_06_VerifyTotalPlan extends BaseTest {
 
     @Test
     public void testTotalPlan(){
-        base = new BaseTest();
-        WebDriver driver = base.getDriver();
+        TravelQuotePage totalPlan = new TravelQuotePage(driver);
+        String total = totalPlan.totalTravelPlan();
+
+        Assert.assertEquals(total, "Showing 16 plans for 2 members");
     }
 }
---
+++ src/test/java/test/TC_08_ApplySorting.java
@@ -0,0 +1,18 @@
+package test;
+
+import basetest.BaseTest;
+import org.policy.pages.TravelQuotePage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_08_ApplySorting extends BaseTest {
+
+    @Test
+    public void testApplyFilter() {
+
+        TravelQuotePage filter = new TravelQuotePage(driver);
+
+        boolean isFilterApply = filter.applyFilter();
+        Assert.assertTrue(isFilterApply, "Filter applied from low to high price");
+    }
+}
