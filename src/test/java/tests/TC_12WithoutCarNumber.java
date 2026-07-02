b45c99c43e8baae02ec67e67ecc5dc4120ecd399
jitabhsin <jitabhsin@gmail.com>
2026-07-02 17:55:26 +0530

    test case 12

---
+++ src/test/java/tests/TC_12WithoutCarNumber.java
@@ -0,0 +1,43 @@
+package tests;
+
+import basetest.BaseTest;
+import org.insurance.pages.CarPage;
+import org.insurance.pages.HomePage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_12WithoutCarNumber extends BaseTest {
+
+    HomePage homePage;
+    CarPage carPage;
+
+    @Test
+    public void verifyErrorWithoutCarNumber() {
+
+        homePage = new HomePage(driver);
+        carPage = new CarPage(driver);
+
+        homePage.clickCarInsurance();
+
+        carPage.clickGetQuote();
+
+        Assert.assertTrue(
+                carPage.isValidationMessageDisplayed(),
+                "Error message is not displayed");
+        System.out.println("✓ Error message displayed successfully");
+
+        String actualMessage =
+                carPage.getValidationMessage();
+
+        Assert.assertEquals(
+                actualMessage,
+                "Please enter a valid reg number",
+                "Incorrect validation message displayed");
+
+        System.out.println(
+                "✓ Validation Message : "
+                        + actualMessage);
+
+        System.out.println("TC_12 PASSED");
+    }
+}
