ed80f5ba797952c6d45b20748813b4362ce74cb4
Patel <2497826@cognizant.com>
2026-07-03 10:56:28 +0530

    health testcase 23 added

--- src/test/java/tests/TC_21_ClickHealthButton.java
+++ src/test/java/tests/TC_21_ClickHealthButton.java
@@ -5,7 +5,7 @@ import org.insurance.pages.HealthHomePage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class TC_21ClickHealthButton extends BaseTest {
+public class TC_21_ClickHealthButton extends BaseTest {
     HealthHomePage healthHomePage;
     @Test
     public void verifyHealthTabButtonsVisibilityAndWorking() {
@@ -13,5 +13,6 @@ public class TC_21ClickHealthButton extends BaseTest {
 
         healthHomePage.clickHealthTab();
         Assert.assertEquals(healthHomePage.isSelectProductDisplayed(), "Select products");
+        System.out.println("Product selected successfully");
     }
 }
