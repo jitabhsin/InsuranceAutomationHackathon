ce5e3f8942e37d583d1e9c7d4e4febd4d42bbbca
jitabhsin <jitabhsin@gmail.com>
2026-07-03 10:59:31 +0530

    commetn

--- src/test/java/tests/TC_06_VerifyTotalPlan.java
+++ src/test/java/tests/TC_06_VerifyTotalPlan.java
@@ -1,17 +1,17 @@
-package tests;
-
-import basetest.BaseTest;
-import org.insurance.pages.TravelQuotePage;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-public class TC_06_VerifyTotalPlan extends BaseTest {
-
-    @Test
-    public void testTotalPlan(){
-        TravelQuotePage totalPlan = new TravelQuotePage(driver);
-        String total = totalPlan.totalTravelPlan();
-
-        Assert.assertEquals(total, "Showing 16 plans for 2 members");
-    }
-}
+//package tests;
+//
+//import basetest.BaseTest;
+//import org.insurance.pages.TravelQuotePage;
+//import org.testng.Assert;
+//import org.testng.annotations.Test;
+//
+//public class TC_06_VerifyTotalPlan extends BaseTest {
+//
+//    @Test
+//    public void testTotalPlan(){
+//        TravelQuotePage totalPlan = new TravelQuotePage(driver);
+//        String total = totalPlan.totalTravelPlan();
+//
+//        Assert.assertEquals(total, "Showing 16 plans for 2 members");
+//    }
+//}
