294b6ffda0d57137a404d8b7c456cc8cc4a6dc76
2497728 <harish.vr.021@gmail.com>
2026-07-03 09:20:49 +0530

    Updated changd element

--- src/test/java/tests/TC_05_SelectHealth.java
+++ src/test/java/tests/TC_05_SelectHealth.java
@@ -32,7 +32,7 @@ public class TC_05_SelectHealth extends BaseTest {
         String selectedCountry = travelHomePage.getSelectedCountry();
         System.out.println("Selected Country: " + selectedCountry);
 
-        travelHomePage.selectStartDateElement.click();
+        travelHomePage.selectStartAndEndDateElement.click();
         travelHomePage.selectStartDate(startDate);
         travelHomePage.selectEndDate(endDate);
         travelHomePage.submitDate();
