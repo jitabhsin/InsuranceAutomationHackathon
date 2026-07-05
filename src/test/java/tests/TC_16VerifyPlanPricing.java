d8d21ffaab93686bbd97090fbd79d45fead7686d
jitabhsin <jitabhsin@gmail.com>
2026-07-05 18:22:24 +0530

    test case 16

--- src/test/java/tests/TC_16VerifyPlanPricing.java
+++ src/test/java/tests/TC_16VerifyPlanPricing.java
@@ -3,9 +3,12 @@ package tests;
 import basetest.BaseTest;
 import org.insurance.pages.CarPage;
 import org.insurance.pages.HomePage;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.ExcelReader;
+
+import java.util.List;
 
 public class TC_16VerifyPlanPricing extends BaseTest {
 
@@ -40,59 +43,74 @@ public class TC_16VerifyPlanPricing extends BaseTest {
         carPage.clickNewVehicleGetQuote();
 
         Assert.assertTrue(carPage.isSelectPlansPageDisplayed(),"Select Plans page not displayed");
-
         carPage.enterCity(city);
         carPage.selectCity(city);
         carPage.enterCarMake(make);
         carPage.selectCarModel(model);
-
         Assert.assertTrue(driver.getCurrentUrl().contains("select-plans"),"User is not on Select Plans page");
-
         carPage.clickProceedBtnCity();
-
-        // card zero dep
+        //  Zero Dep
         boolean zeroDepSelected = carPage.selectZeroDepIfNotSelected();
         System.out.println("Zero Dep selected : " + zeroDepSelected);
         Assert.assertTrue(zeroDepSelected,"Zero Dep plan is not selected");
+        verifyPlan("Zero Dep", carPage.getZeroDepAmount(),
+                carPage.getTotalPremiumAmount(), carPage.getPlanAmount());
+        loopPersonalProtectSums("Zero Dep");
 
-        verifyPlan("Zero Dep", carPage.getZeroDepAmount(), carPage.getTotalPremiumAmount(),
-                carPage.getPlanAmount());
-
-        carPage.togglePersonalProtect();
-        verifyAfterAddon("Zero Dep", carPage.getTotalPremiumAmount(), carPage.getPlanAmount());
-        carPage.togglePersonalProtect();
-
-        // card No Frills
+        // No Frills
         boolean noFrillsSelected = carPage.selectNoFrillsIfNotSelected();
         System.out.println("No Frills selected : " + noFrillsSelected);
         Assert.assertTrue(noFrillsSelected,"No Frills plan is not selected");
-
-        verifyPlan("No Frills",
-                carPage.getNoFrillsAmount(),
-                carPage.getTotalPremiumAmount(),
-                carPage.getPlanAmount());
-
-        carPage.togglePersonalProtect();
-        verifyAfterAddon("No Frills", carPage.getTotalPremiumAmount(), carPage.getPlanAmount());
-        carPage.togglePersonalProtect();
-
-        // card smart cover
+        verifyPlan("No Frills", carPage.getNoFrillsAmount(),
+                carPage.getTotalPremiumAmount(), carPage.getPlanAmount());
+        loopPersonalProtectSums("No Frills");
+        // Smart Cover
         boolean smartCoverSelected = carPage.selectSmartCoverIfNotSelected();
         System.out.println("Smart Cover selected : " + smartCoverSelected);
         Assert.assertTrue(smartCoverSelected,"Smart Cover plan is not selected");
 
-        verifyPlan("Smart Cover",
-                carPage.getSmartCoverAmount(),
-                carPage.getTotalPremiumAmount(),
-                carPage.getPlanAmount());
+        verifyPlan("Smart Cover", carPage.getSmartCoverAmount(),
+                carPage.getTotalPremiumAmount(), carPage.getPlanAmount());
+        loopPersonalProtectSums("Smart Cover");
+    }
 
-        carPage.togglePersonalProtect();
-        verifyAfterAddon("Smart Cover", carPage.getTotalPremiumAmount(), carPage.getPlanAmount());
-        carPage.togglePersonalProtect();
+    private void loopPersonalProtectSums(String planName) {
+        String baseTotal = carPage.getTotalPremiumAmount();
+        System.out.println("Base Total for " + planName + " : " + baseTotal);
+        carPage.openPersonalProtectDropdown();
+        int optionCount = carPage.getPersonalProtectSumOptions().size();
+        System.out.println("Sum Assured options found for " + planName + " : " + optionCount);
+
+        for (int i = 0; i < optionCount; i++) {
+            carPage.openPersonalProtectDropdown();
+            List<WebElement> options = carPage.getPersonalProtectSumOptions();
+            WebElement option = options.get(i);
+            String optionLabel = option.getText().trim();
+
+            carPage.selectPersonalProtectSum(option);
+            System.out.println(planName + " | Sum Assured selected : " + optionLabel);
+
+            carPage.togglePersonalProtect();
+            String totalAfterAdd = carPage.getTotalPremiumAmount();
+            String planAfterAdd  = carPage.getPlanAmount();
+            verifyAfterAddon(planName + " [" + optionLabel + "]", totalAfterAdd, planAfterAdd);
+
+            carPage.togglePersonalProtect();
+            String totalAfterRemove = carPage.getTotalPremiumAmount();
+            System.out.println(planName + " | After Remove Total : " + totalAfterRemove);
+
+            Assert.assertEquals(
+                    totalAfterRemove.replaceAll("[^0-9,]",""),
+                    baseTotal.replaceAll("[^0-9,]",""),
+                    planName + " Total did not reset after removing addon [" + optionLabel + "]"
+            );
+        }
+
+        System.out.println( planName + " Personal Protect loop complete ");
     }
 
     private void verifyPlan(String planName, String cardAmount, String totalPremium, String planAmount) {
-        System.out.println( planName +" : ");
+        System.out.println(planName + " : ");
         System.out.println(planName + " Card Amount   : " + cardAmount);
         System.out.println("Total Premium Amount : " + totalPremium);
         System.out.println("Plan Amount          : " + planAmount);
