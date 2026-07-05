1135ef393ff4dcdacfe35b85ce37f7c304b2cdbf
2497728 <harish.vr.021@gmail.com>
2026-07-05 23:40:04 +0530

    New Testcases added and old ones updated Travel

--- src/test/java/tests/TC_04_VerifySelectedCountry.java
+++ src/test/java/tests/TC_04_VerifySelectedCountry.java
@@ -3,23 +3,21 @@ package tests;
 import basetest.BaseTest;
 import org.insurance.pages.HomePage;
 import org.insurance.pages.TravelHomePage;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import utils.ConfigReader;
-import utils.ExcelReader;
 
-public class TC_03_SelectDates extends BaseTest {
-
+public class TC_04_VerifySelectedCountry extends BaseTest {
     HomePage homePage;
     TravelHomePage travelHomePage;
 
     @Test
-    public void selectDates(){
-
+    public void verifyAndDisplaySelectedCountry(){
         homePage = new HomePage(driver);
         travelHomePage = new TravelHomePage(driver);
+        SoftAssert softAssert = new SoftAssert();
 
         homePage.clickTravelInsurance();
         homePage.clickTravelScope();
@@ -29,6 +27,15 @@ public class TC_03_SelectDates extends BaseTest {
         travelHomePage.selectCountry(ConfigReader.getProperty("country"));
         String selectedCountry = travelHomePage.getSelectedCountry();
         System.out.println("Selected Country: " + selectedCountry);
+        boolean isDisplayed = travelHomePage.isSelectTravelTypeVisible();
+        boolean isClicked = travelHomePage.isSelectTravelTypeSelected();
+        boolean isSelected = travelHomePage.isCountrySelectedCorrectly();
+
+        softAssert.assertTrue(isDisplayed, "Select Country Type not Visible");
+        softAssert.assertTrue(isClicked, "Select Country Type has not been Selected");
+        softAssert.assertTrue(isSelected, "Wrong Country has been selected");
+        softAssert.assertEquals(selectedCountry, ConfigReader.getProperty("country"), "Selected country does not match expected country");
+        softAssert.assertAll();
 
         travelHomePage.selectStartAndEndDateElement.click();
         boolean isCalenderOpen = travelHomePage.isCalenderOpen();
@@ -40,13 +47,13 @@ public class TC_03_SelectDates extends BaseTest {
 
         boolean verifyTravelCountRedirection = travelHomePage.isRedirectedToSelectTravellerCount();
 
-        Assert.assertTrue(isCalenderOpen, "Calendar NOT Opened");
-        Assert.assertTrue(isDateSubmitPresent, "Date Submit Button NOT Present");
-
         String termsStatus = travelHomePage.getTermsStatus();
-        SoftAssert softAssert = new SoftAssert();
         softAssert.assertEquals(termsStatus, "UNCHECKED", "Terms checkbox should be unchecked");
 
+        Assert.assertTrue(isCalenderOpen, "Calendar NOT Opened");
+        Assert.assertTrue(isDateSubmitPresent, "Date Submit Button NOT Present");
         Assert.assertTrue(verifyTravelCountRedirection, "Traveller Count Selection NOT loaded");
+
     }
+
 }
